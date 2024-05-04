85c49b7ed9085e741bf2aa979682481059728579
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2024-05-04 11:35:45 +0200

    update deps, improve satisfy schema

--- src/libraries/shared/src/SchemaSatisfy.ts
+++ src/libraries/shared/src/SchemaSatisfy.ts
@@ -1,35 +1,78 @@
-import { Schema } from "@effect/schema";
+import type { Simplify } from "effect/Types";
+import type { Schema } from "@effect/schema/Schema";
 
 import { type Json } from "./Schema.js";
 
 const encoded =
   <Encoded>() =>
-  <Context, Type>(schema: Schema.Schema<Type, Encoded, Context>) =>
+  <X extends Schema.Any>(
+    schema: Schema.Encoded<X> extends Encoded
+      ? X
+      : {
+          expected: Simplify<Encoded>;
+          received: Simplify<Schema.Encoded<X>>;
+        }
+  ) =>
     schema;
 
 encoded.json =
   <Encoded extends Json.Json>() =>
-  <Context, Type>(schema: Schema.Schema<Type, Encoded, Context>) =>
+  <X extends Schema.Any>(
+    schema: Schema.Encoded<X> extends Encoded
+      ? X
+      : {
+          expected: Simplify<Encoded>;
+          received: Simplify<Schema.Encoded<X>>;
+        }
+  ) =>
     schema;
 
 const type =
   <Type>() =>
-  <Context, Encoded>(schema: Schema.Schema<Type, Encoded, Context>) =>
+  <X extends Schema.Any>(
+    schema: Schema.Type<X> extends Type
+      ? X
+      : {
+          expected: Simplify<Type>;
+          received: Simplify<Schema.Type<X>>;
+        }
+  ) =>
     schema;
 
 type.json =
   <Type extends Json.Json>() =>
-  <Context, Encoded>(schema: Schema.Schema<Type, Encoded, Context>) =>
+  <X extends Schema.Any>(
+    schema: Schema.Type<X> extends Type
+      ? X
+      : {
+          expected: Simplify<Type>;
+          received: Simplify<Schema.Type<X>>;
+        }
+  ) =>
     schema;
 
 const context =
   <Context>() =>
-  <Type, Encoded>(schema: Schema.Schema<Type, Encoded, Context>) =>
+  <X extends Schema.Any>(
+    schema: Schema.Context<X> extends Context
+      ? X
+      : {
+          expected: Simplify<Context>;
+          received: Simplify<Schema.Context<X>>;
+        }
+  ) =>
     schema;
 
 context.json =
   <Context extends Json.Json>() =>
-  <Type, Encoded>(schema: Schema.Schema<Type, Encoded, Context>) =>
+  <X extends Schema.Any>(
+    schema: Schema.Context<X> extends Context
+      ? X
+      : {
+          expected: Simplify<Context>;
+          received: Simplify<Schema.Context<X>>;
+        }
+  ) =>
     schema;
 
 export const satisfies = { encoded, type, context };
