105654cd16fbd798d7470db293e2fca01937d712
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2024-08-07 17:24:53 +0200

    update deps

--- src/libraries/workbench/src/index.ts
+++ src/libraries/workbench/src/index.ts
@@ -1,39 +1,6 @@
-import { Config, Console, Effect, Redacted, Logger } from "effect";
+import * as OptionInstances from "@effect/typeclass/data/Option";
+import * as RecordInstances from "@effect/typeclass/data/Record";
 
-const getJson = ({ url }: { url: string }) =>
-  Effect.tryPromise(() =>
-    fetch(url).then((res) => {
-      if (!res.ok) {
-        console.log("error");
-        throw new Error(res.statusText);
-      }
-      console.log("ok");
-      return res.json() as unknown;
-    })
-  ).pipe(
-    Effect.tapErrorCause(Effect.log),
-    Effect.retry({ times: 2 }),
-    Effect.timeout("41 seconds"),
-    Effect.catchAll(Console.error)
-  );
-
-const program = Effect.gen(function* () {
-  const host = "https://dummyjson.com/auth/products/1?delay=1000";
-
-  const unknownResponse = yield* getJson({
-    url: host,
-  });
-
-  console.log(unknownResponse);
-}).pipe(
-  Effect.provide(
-    Logger.replace(
-      Logger.defaultLogger,
-      Logger.prettyLogger({
-        colors: true,
-      })
-    )
-  )
+const traverse = RecordInstances.Traversable.traverse(
+  OptionInstances.Applicative
 );
-
-Effect.runPromise(program);
