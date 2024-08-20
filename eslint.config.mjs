f15d5b9711e0e0ca832b84456aee5405af227355
maksim.khramtsov <maksim.khramtsov@btsdigital.kz>
2024-08-20 10:53:35 +0200

    update deps, eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,98 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+// import functional from "eslint-plugin-functional";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import parser from "vue-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/dist/**/*"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:@typescript-eslint/stylistic",
+    // "plugin:functional/no-exceptions",
+    // "plugin:typescript-sort-keys/recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended"
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      // functional,
+      prettier,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: parser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".vue"],
+        project: true,
+      },
+    },
+
+    rules: {
+      //   "sort-keys": ["error"],
+      "@typescript-eslint/no-empty-interface": ["off"],
+      //   "functional/readonly-type": ["error", "keyword"],
+      "@typescript-eslint/method-signature-style": ["error", "property"],
+
+      //   "@typescript-eslint/ban-types": [
+      //     "error",
+      //     {
+      //       types: {
+      //         "{}": false,
+      //       },
+
+      //       extendDefaults: true,
+      //     },
+      //   ],
+
+      "@typescript-eslint/consistent-type-definitions": "off",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      "@typescript-eslint/no-explicit-any": "off",
+
+      "@typescript-eslint/array-type": [
+        "error",
+        {
+          default: "generic",
+          readonly: "generic",
+        },
+      ],
+
+      "@typescript-eslint/no-unnecessary-condition": "off",
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "require-yield": "off",
+    },
+  },
+];
--- src/libraries/workbench/src/index.ts
+++ src/libraries/workbench/src/index.ts
@@ -1,6 +1,46 @@
-import * as OptionInstances from "@effect/typeclass/data/Option";
-import * as RecordInstances from "@effect/typeclass/data/Record";
+import { Effect, Ref } from "effect";
 
-const traverse = RecordInstances.Traversable.traverse(
-  OptionInstances.Applicative
-);
+const doWork = (options: {
+  //
+  delay: number;
+  result: number;
+  ref: Ref.Ref<Array<number>>;
+}) =>
+  Effect.sleep(options.delay).pipe(
+    Effect.andThen(
+      Ref.update(options.ref, (value) => {
+        value.push(options.result);
+        return value;
+      })
+    )
+  );
+
+let delayStart = 0;
+function getDelayHappy() {
+  const oldDelay = delayStart;
+  delayStart += 100;
+  return oldDelay;
+}
+
+function getDelayRealLife() {
+  return Math.random() * 100;
+}
+
+const buggyProgramm = Effect.gen(function* () {
+  const resultRef = yield* Ref.make<Array<number>>([]);
+
+  yield* Effect.all(
+    [
+      doWork({ ref: resultRef, result: 1, delay: getDelayHappy() }),
+      doWork({ ref: resultRef, result: 2, delay: getDelayHappy() }),
+      doWork({ ref: resultRef, result: 3, delay: getDelayHappy() }),
+    ],
+    { concurrency: "unbounded" }
+  );
+
+  const result = yield* Ref.get(resultRef);
+
+  yield* Effect.log(result);
+});
+
+buggyProgramm.pipe(Effect.runPromise);
